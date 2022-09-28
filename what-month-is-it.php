f97ba108e4b44b191ab5b5470e28ae9391147808
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-28 09:57:48 +0200

    exercice 6 quest 2 nth try ??

---
+++ what-month-is-it.php
@@ -0,0 +1,57 @@
+<?php
+$month = 0;
+$nameOfMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+switch ($month) {
+    case 0:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 1:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 2:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 3:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 4:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 5:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 6:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 7:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 8:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 9:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 10:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 11:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+    case 12:
+        echo "We are in the month of $nameOfMonth[$month]";
+        $month++;
+        break;
+}
