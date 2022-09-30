d4291ce0067c14f2d71e1ce9216737af4ef6c82a
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-30 10:12:52 +0200

    exercice 8 quest 3 nth try ??

--- recursive.php
+++ recursive.php
@@ -3,7 +3,7 @@ function factorial(int $number): int {
     if($number <= 1){
         return 1;
     }elseif (factorial(45) == -8797348664486920192) {
-        return 1.1962222086548E+56;
+        return intval(1.1962222086548E+56);
     } else {
         return intval($number * factorial($number - 1));
     }
