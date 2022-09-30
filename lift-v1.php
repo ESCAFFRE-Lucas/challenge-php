a0c9aa3dc1eb2e86295468639740c3e0787960d6
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-30 09:43:19 +0200

    exercice 7 quest 3 nth try ??

---
+++ lift-v1.php
@@ -0,0 +1,14 @@
+<?php
+function getFloor(int $currentFloor, int|null $requestedFloor, array $listOfButton): int {
+    return $requestedFloor;
+}
+
+function getDirection(int $currentFloor, int|null $requestedFloor, array $listOfButton): int {
+    if ($currentFloor < $requestedFloor) {
+        return 1;
+    } elseif ($currentFloor == $requestedFloor) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
