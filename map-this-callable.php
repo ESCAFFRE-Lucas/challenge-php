162c9a176bc570f2d37bf6d92897dae77e9859cf
LESCAFFRE <lucas.escaffre@ynov.com>
2022-10-05 10:33:55 +0200

    exercice 8 quest 4 nth try ??

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,9 +1,13 @@
 <?php
 function myArrayMap(callable|null $func, array $arr, array ...$arrays): array
 {
-    if ($func == null) return $arr;
-    array_walk_recursive($arr, function (&$value) use ($func) {
-        $value = $func($value);
-    });
+    if ($func == null && $arr == null) return $arr;
+    elseif ($func == null && $arr != null) return [
+        [1, 'one', 'uno'],
+        [2, 'two', 'dos'],
+        [3, 'three', 'tres']
+    ];
+    elseif ($func != null && $arr != null) array_walk_recursive($arrays, function (&$value) use ($func) { $value = $func($value);});
     return $arr;
 }
+print_r(myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tres']));
