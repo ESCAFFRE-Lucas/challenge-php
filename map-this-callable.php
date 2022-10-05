0ba4419d5ae1dfc0ef9653041c18d60b02b0b219
LESCAFFRE <lucas.escaffre@ynov.com>
2022-10-05 10:20:54 +0200

    exercice 8 quest 4 nth try ??

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,5 +1,5 @@
 <?php
-function myArrayMap(callable $func, array $arr, array ...$arrays): ?array
+function myArrayMap(callable|null $func, array $arr, array ...$arrays): ?array
 {
     if ($func == null) return null;
     array_walk_recursive($arr, function (&$value) use ($func) {
