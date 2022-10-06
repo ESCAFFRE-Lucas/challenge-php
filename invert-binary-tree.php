f17ac9f3021ec00437b6b2217eba9a7d50fd17cd
LESCAFFRE <lucas.escaffre@ynov.com>
2022-10-06 21:45:55 +0200

    exercice 7 quest 5 nth try ??

--- invert-binary-tree.php
+++ invert-binary-tree.php
@@ -1,12 +1,11 @@
 <?php
-class noBug {
-    public function invertTree (BinaryNode $binaryTree): BinaryNode {
-        $stock = $binaryTree->left;
-        $binaryTree->left = $binaryTree->right;
-        $binaryTree->right = $stock;
-        $binaryTree->invertTree($binaryTree->left);
-        $binaryTree->invertTree($binaryTree->right);
-        return $binaryTree;
-    }
+function invertTree(BinaryNode $binaryTree): BinaryNode
+{
+    $stock = $binaryTree->left;
+    $binaryTree->left = $binaryTree->right;
+    $binaryTree->right = $stock;
+    $binaryTree->invertTree($binaryTree->left);
+    $binaryTree->invertTree($binaryTree->right);
+    return $binaryTree;
 }
 
